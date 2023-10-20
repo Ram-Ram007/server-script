5e186c8d5096b8f1774f60b6f96bef9d8cc4aa4a
Ram-Ram007 <ramajayam2411@gmail.com>
2023-10-20 22:07:04 +0530

    read method

--- app/index.js
+++ app/index.js
@@ -2,3 +2,18 @@ const express = require("express");
 const morgan = require("morgan");
 const config = require("./config");
 
+const app = express();
+app.use(morgan("combined"));
+
+app.use(express.json());
+
+const favs = [];
+
+// reading READ
+app.get("/", (req, res) => {
+  return res.json({
+    data: {
+      favs,
+    },
+  });
+});
